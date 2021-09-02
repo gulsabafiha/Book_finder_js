3f0d0b493171ca8c8c975fe5f46b3adf3fc71b59
gulsaba fiha <gulsabafiha001@gmail.com>
2021-09-02 14:20:27 +0600

    intendation

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ const getApi = () => {
     }
 }
 const getDetails = data => {
-    console.log(data);
     if (data.numFound === 0) {
         const div = document.createElement('div');
         div.innerHTML = `
@@ -30,25 +29,25 @@ const getDetails = data => {
         const dataDoc = data.docs;
         mainDiv.innerHTML = '';
         dataDoc.forEach(item => {
-                const imgUrl = `https://covers.openlibrary.org/b/id/${item.cover_i}-M.jpg`;
-                    const div = document.createElement('div');
-                    div.innerHTML = `
-                        <div class="card mb-3 mx-auto shadow p-3 mb-5 bg-body rounded" style="max-width: 540px;">
-                            <div class="row g-0">
-                                <div class="col-md-4">
-                                        <img src="${imgUrl}" class="img-fluid rounded-start" alt="Book Image ThumbNail">
-                                </div>
-                                <div class="col-md-8">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Book Name: <span class="text-success">${item.title}</span></h5>
-                                        <h6 class="card-text mt-2">by: <span class="text-info">${item.author_name[0]}</span></h6>
-                                        <p class="card-text">Publisher: <small class="text-muted">${item.publisher[0]}</small></p>
-                                        <p class="card-text">Publish Date: <small class="text-muted">${item.publish_date[0]}</small></p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>`   
-                    mainDiv.appendChild(div);
+        const imgUrl = `https://covers.openlibrary.org/b/id/${item.cover_i}-M.jpg`;
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div class="card mb-3 mx-auto shadow p-3 mb-5 bg-body rounded" style="max-width: 540px;">
+                 <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="${imgUrl}" class="img-fluid rounded-start" alt="Book Image ThumbNail">
+                    </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Book Name: <span class="text-success">${item.title}</span></h5>
+                        <h6 class="card-text mt-2">by: <span class="text-info">${item.author_name[0]}</span></h6>
+                        <p class="card-text">Publisher: <small class="text-muted">${item.publisher[0]}</small></p>
+                        <p class="card-text">Publish Date: <small class="text-muted">${item.publish_date[0]}</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+        mainDiv.appendChild(div);
         })
-    } 
-}
+    }
+} 
